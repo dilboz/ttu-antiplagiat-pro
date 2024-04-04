1a69154a4bd87b9cefc5b7270e917e18ac225189
dilboz <dilbozsafarov@gmail.com>
2024-04-04 17:02:03 +0500

    feat: log micreation

---
+++ contracts/LogService/ILoggerManager.cs
@@ -0,0 +1,9 @@
+﻿namespace contracts.LogService;
+
+public interface ILoggerManager
+{
+    void LogInfo(string message);
+    void LogWarn(string message);
+    void LogDebug(string message);
+    void LogError(string message);
+}
